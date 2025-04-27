a0e1260904f74f987ca3a05b17e425e134ae89fd
sidsurakanti <sidsurakanti1@gmail.com>
2025-04-27 03:10:54 -0400

    feat: 83% accuracy with new model architecture + data aug

--- nn.py
+++ nn.py
@@ -1,75 +1,85 @@
-# %%
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from torchvision.transforms.v2 import ToImage, ToDtype
+from torchvision.transforms.v2 import ToImage, ToDtype, Normalize, RandomAffine
 
 from matplotlib import pyplot as plt
 import numpy as np
+import time
 
-# %%
+
 print(torch.cuda.is_available(), torch.cuda.get_device_name())
 
-# %%
-transforms = transforms.Compose([
+train_transforms = transforms.Compose([
     ToImage(),
-    ToDtype(torch.float32, scale=True)
+    ToDtype(torch.float32, scale=True),
+    Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    RandomAffine(
+      degrees=10,
+      translate=(0.15, 0.15),
+      scale=(0.9, 1.1)
+    )
 ])
 
-# %%
-trainD = datasets.CIFAR10(root="data", train=True, transform=transforms, download=True)
-testD = datasets.CIFAR10(root="data", train=False, transform=transforms, download=True)
+test_transforms = transforms.Compose([
+  ToImage(),
+  ToDtype(torch.float32, scale=True),
+  Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
 
-# %%
-x, label = trainD[np.random.randint(len(trainD))] # 32x32 PIL image, label
-plt.imshow(x.permute(1, 2, 0))
-print(label, x.size(), type(x))
+trainD = datasets.CIFAR10(root="data", train=True, transform=train_transforms, download=True)
+testD = datasets.CIFAR10(root="data", train=False, transform=test_transforms, download=True)
 
-# %%
-BATCH_SIZE = 512
+BATCH_SIZE = 256
 DEVICE = torch.accelerator.current_accelerator()
 
 trainDL = DataLoader(trainD, batch_size=BATCH_SIZE, shuffle=True, num_workers=8, pin_memory=True)
-testDL = DataLoader(trainD, batch_size=BATCH_SIZE, shuffle=True, num_workers=8, pin_memory=True)
+testDL = DataLoader(testD, batch_size=BATCH_SIZE, shuffle=False, num_workers=8, pin_memory=True)
 
-# %%
 class Cifar(nn.Module):
   def __init__(self):
     super().__init__()
     self.labels = {
-                    0: "airplane",  
-                    1: "automobile", 
-                    2: "bird",
-                    3: "cat",
-                    4: "deer",
-                    5: "dog",
-                    6: "frog",
-                    7: "horse",
-                    8: "ship",
-                    9: "truck"
-                  }
+      0: "airplane",  
+      1: "automobile", 
+      2: "bird",
+      3: "cat",
+      4: "deer",
+      5: "dog",
+      6: "frog",
+      7: "horse",
+      8: "ship",
+      9: "truck"
+    }
     self.model = nn.Sequential(
-      nn.Conv2d(3, 10, 3, padding=1),
+      nn.Conv2d(3, 64, 3, padding=1), # 32x32x64
       nn.ReLU(),
-      nn.MaxPool2d(2, 2),
 
-      nn.Conv2d(10, 20, 5),
+      nn.Conv2d(64, 128, 3), # 29x29x128
       nn.ReLU(),
-      nn.MaxPool2d(2, 2),
+      nn.MaxPool2d(2, 2), # 14x14x128
+
+      nn.Conv2d(128, 256, 3, padding=1), # 14x14x256
+      nn.ReLU(),
+
+      nn.Conv2d(256, 216, 5), # 10x10x216 
+      nn.ReLU(),
+      nn.MaxPool2d(2, 2), # 5x5x216
 
       nn.Flatten(),
-      nn.Linear(720, 256),
+      nn.Linear(5*5*216, 512),
       nn.ReLU(),
+      nn.Dropout(0.3),
 
-      nn.Linear(256, 10)
+      nn.Linear(512, 10)
     )
 
   def forward(self, x):
     logits = self.model(x)
     return logits
 
-# %%
+
 def train(dataloader, model, loss_fn, optimizer):
   model.train()
   
@@ -85,8 +95,9 @@ def train(dataloader, model, loss_fn, optimizer):
     optimizer.step()    
     optimizer.zero_grad()
 
-    if batch_i % 25 < 1:
-      print(f"Batch {batch_i}/{len(dataloader)}, Loss: {loss.item():.4f}")
+    if (batch_i % 50 < 1) or (batch_i == len(dataloader) - 1):
+      print(f"Batch {batch_i+1}/{len(dataloader)}, Loss: {loss.item():.4f}")
+
 
 def test(dataloader, model, loss_fn):
   model.eval()
@@ -112,9 +123,8 @@ def test(dataloader, model, loss_fn):
   return accuracy, avg_loss
   
 
-def fit(epochs: int):
-  model = Cifar().to(DEVICE)
-  optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+def fit(model, epochs: int):
+  optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, weight_decay=1e-4)
   loss_fn = torch.nn.CrossEntropyLoss()
 
   accuracies, losses = [], []
@@ -134,26 +144,54 @@ def fit(epochs: int):
   return accuracies, losses
 
 
-# %%
-EPOCHS = 50
+EPOCHS = 20
+model = Cifar().to(DEVICE)
 
-acc, loss = fit(EPOCHS)
+acc, loss = fit(model, EPOCHS)
 
 
-# %%
-fig, (ax1, ax2) = plt.subplots(2)
+fig, axs = plt.subplots(ncols=2, figsize=(9, 3), layout="constrained")
 fig.suptitle("Performance")
 
 epochs = range(1, EPOCHS+1)
 
-print(acc)
-ax1.plot(epochs, acc)
+ax1, ax2 = axs[0], axs[1]
+
+ax1.plot(epochs, acc, "tab:purple")
 ax1.set_title("Accuracy")
+ax1.set_xlabel("Epochs")
+ax1.grid(True)
 
-ax2.plot(epochs, loss)
-ax1.set_title("Loss")
+ax2.plot(epochs, loss, "tab:orange")
+ax2.set_title("Loss")
+ax2.set_xlabel("Epochs")
+ax2.grid(True)
 
-# %%
+plt.savefig(f"performance_{int(time.time())}")
+
+def predict(model):
+  model.eval()
+  fig, axs = plt.subplots(5, 1, layout="constrained")
+
+  for i in range(5):
+    X, label = testD[np.random.randint(len(testD))] # tensor image, label
+    
+    unnorm = Normalize(mean=[-1.0, -1.0, -1.0], std=[2.0, 2.0, 2.0])
+    img = unnorm(X)
+    axs[i].imshow(img.permute(1, 2, 0))
+
+    X = X.unsqueeze(0).to(DEVICE)
+    
+    with torch.no_grad():
+      pred = model(X).argmax(dim=1).item()
+    axs[i].set_title(f"P: {model.labels[pred]}, T: {model.labels[label]}") 
+    axs[i].yaxis.set_visible(False)
+    axs[i].xaxis.set_visible(False)
+  
+  plt.show()
+  
+
+predict(model)
 
 
 
